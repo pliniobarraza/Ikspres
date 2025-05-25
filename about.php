6d444a091d332e955b13ffc0afd18757dd6f2336
Plinio Barraza <barrazaplinio@gmail.com>
2025-05-25 13:10:08 -0500

    Update about.php

--- about.php
+++ about.php
@@ -1,14 +1,14 @@
 <div style="margin:0 auto;"> <br />
     <fieldset style="padding: 10px; border: 2px solid #000;">
-        <legend>Template Plugin Info</legend>
+        <legend>FPP Ikspres</legend>
         <div style="overflow: hidden; padding: 10px;">
             <div id='credits'>
-                <b>Template Plugin Developed By:</b><br />
+                <b>FPP-Ikspres Developed By:</b><br />
                 <br />
-                John Doe (jdoe)<br />
+                Plinio Barraza<br />
                 <br />
-                <a href='https://github.com/FalconChristmas/fpp-plugin-Template'>Git Repository</a><br>
-                <a href='https://github.com/FalconChristmas/fpp-plugin-Template/issues'>Bug Reporter</a><br>
+                <a href='http://pliniobarraza.com'>Personal Site</a><br>
+                <a href='https://github.com/pliniobarraza/Ikspres'>GitHub</a><br>
                 <br />
             </div>
         </div>
