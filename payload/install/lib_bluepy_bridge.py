2c7cb673a782ab69692b5fee1e20adad6bfc0240
Plinio Barraza <barrazaplinio@gmail.com>
2025-05-26 03:45:16 -0500

    Update lib_bluepy_bridge.py

--- payload/install/lib_bluepy_bridge.py
+++ payload/install/lib_bluepy_bridge.py
@@ -5,22 +5,13 @@ import atexit
 import bluepy.btle as btle
 import serial
 import sys
-
 ser = serial.Serial(
     port='/dev/ttyAMA2',
     baudrate=115200,
 )
-
 p = btle.Peripheral("9C:1D:58:13:D1:90")
 s = p.getServiceByUUID("0000ffe0-0000-1000-8000-00805f9b34fb")
 c = s.getCharacteristics()[0]
-
 while True:
-
-#    print(  bytearray(ser.read(3))[0]  )
-
  c.write(bytearray(ser.read(3)),withResponse=False)
-# c.write(bytearray([i,i,i]),withResponse=False)
-# c.write(bytearray([i,i,i]),withResponse=False)
 
-# time.sleep(.01)
