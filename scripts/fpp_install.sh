65a3c87fd912b233504c07dccd1588a664fda19e
Plinio Barraza <barrazaplinio@gmail.com>
2025-07-19 18:53:17 -0500

    Update fpp_install.sh

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-/usr/bin/socat -u -d -d pty,link=/dev/ttyAMA1,echo=0,perm=0777 pty,link=/dev/ttyAMA2,echo=0,perm=0777
+#do this once, if already done do not do when updating plugin...  or remove when uninstalling
+
+#/usr/bin/socat -u -d -d pty,link=/dev/ttyAMA1,echo=0,perm=0777 pty,link=/dev/ttyAMA2,echo=0,perm=0777
