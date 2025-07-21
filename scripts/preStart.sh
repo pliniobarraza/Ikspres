52a188722769fdcbbde8acd452c393c7adccba7b
Plinio Barraza <barrazaplinio@gmail.com>
2025-07-20 20:37:34 -0500

    Update preStart.sh

--- scripts/preStart.sh
+++ scripts/preStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #SERIAL RELAY
-/usr/bin/socat -u -d -d pty,link=/dev/ttyAMA1,echo=0,perm=0777 pty,link=/dev/ttyAMA2,echo=0,perm=0777
+#/usr/bin/socat -u -d -d pty,link=/dev/ttyAMA1,echo=0,perm=0777 pty,link=/dev/ttyAMA2,echo=0,perm=0777
 
